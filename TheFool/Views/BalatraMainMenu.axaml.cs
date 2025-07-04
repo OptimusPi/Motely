d1bbc2f05ce2540e16ee211f37519de4d250a126
Nathanial P. Howard <admin@optimuspi.com>
2025-07-03 21:02:34 -0500

    main menu gettin there sorta

---
+++ TheFool/Views/BalatraMainMenu.axaml.cs
@@ -0,0 +1,119 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using Avalonia.VisualTree;
+using System;
+
+namespace TheFool.Views
+{
+    public partial class BalatraMainMenu : UserControl
+    {
+        private TextBlock? _profileName;
+        private Grid? _modalContainer;
+        private Border? _searchModal;
+        private string _playerName = "Jimbo";
+        private Border? _profileButton;
+
+        public BalatraMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            
+            _profileName = this.FindControl<TextBlock>("ProfileName");
+            _modalContainer = this.FindControl<Grid>("ModalContainer");
+            _searchModal = this.FindControl<Border>("SearchModal");
+            
+            // Find the profile button border
+            var bottomMenu = this.FindControl<Grid>("BottomMenu");
+            if (bottomMenu != null)
+            {
+                foreach (var child in bottomMenu.Children)
+                {
+                    if (child is Border border && border.Classes.Contains("profile-button"))
+                    {
+                        _profileButton = border;
+                        border.PointerPressed += OnProfileClick;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void OnProfileClick(object? sender, PointerPressedEventArgs e)
+        {
+            // TODO: Show profile modal or handle profile editing
+            Console.WriteLine($"Profile clicked! Current name: {_playerName}");
+        }
+        
+        private void OnHelpClick(object? sender, PointerPressedEventArgs e)
+        {
+            // TODO: Show help modal
+            Console.WriteLine("Help clicked!");
+        }
+
+        private void OnSearchClick(object? sender, RoutedEventArgs e)
+        {
+            ShowModal(_searchModal);
+        }
+
+        private void OnOptionsClick(object? sender, RoutedEventArgs e)
+        {
+            // TODO: Show options modal
+            Console.WriteLine("Options clicked!");
+        }
+
+        private void OnQuitClick(object? sender, RoutedEventArgs e)
+        {
+            if (Application.Current?.ApplicationLifetime is Avalonia.Controls.ApplicationLifetimes.IClassicDesktopStyleApplicationLifetime desktop)
+            {
+                desktop.Shutdown();
+            }
+        }
+
+        private void OnFunRunClick(object? sender, RoutedEventArgs e)
+        {
+            // TODO: Show collection modal
+            Console.WriteLine("Collection clicked!");
+        }
+
+        private void OnStartSearchClick(object? sender, RoutedEventArgs e)
+        {
+            // Navigate to the search interface
+            if (this.GetVisualRoot() is MainWindow mainWindow)
+            {
+                mainWindow.ShowTabbedInterface();
+            }
+            Console.WriteLine($"Start Search clicked! Author: {_playerName}");
+        }
+
+        private void OnManageFiltersClick(object? sender, RoutedEventArgs e)
+        {
+            // TODO: Show filter management interface
+            Console.WriteLine($"Manage Filters clicked! Author: {_playerName}");
+        }
+
+        private void CloseModal(object? sender, RoutedEventArgs e)
+        {
+            if (_searchModal != null)
+            {
+                _searchModal.IsVisible = false;
+            }
+        }
+
+        private void ShowModal(Border? modal)
+        {
+            if (modal != null)
+            {
+                modal.IsVisible = true;
+            }
+        }
+
+        public string GetPlayerName() => _playerName;
+    }
+}
--- TheFool/Views/MainWindow.axaml.cs
+++ TheFool/Views/MainWindow.axaml.cs
@@ -1,12 +1,49 @@
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using TheFool.Controls;
 
 namespace TheFool.Views;
 
 public partial class MainWindow : Window
 {
+    private Grid? _mainGrid;
+    private BalatraMainMenu? _mainMenu;
+    private Grid? _contentGrid;
+    
     public MainWindow()
     {
         AvaloniaXamlLoader.Load(this);
+        InitializeMainMenu();
+    }
+    
+    private void InitializeMainMenu()
+    {
+        // Find the main grid that contains the background
+        _mainGrid = this.FindControl<Grid>("MainGrid");
+        if (_mainGrid == null) return;
+        
+        // Find the content grid (the one with tabs)
+        _contentGrid = this.FindControl<Grid>("ContentGrid");
+        if (_contentGrid != null)
+        {
+            // Hide the tabbed interface initially
+            _contentGrid.IsVisible = false;
+        }
+        
+        // Create and add the main menu
+        _mainMenu = new BalatraMainMenu();
+        _mainGrid.Children.Add(_mainMenu);
+    }
+    
+    public void ShowTabbedInterface()
+    {
+        if (_mainMenu != null) _mainMenu.IsVisible = false;
+        if (_contentGrid != null) _contentGrid.IsVisible = true;
+    }
+    
+    public void ShowMainMenu()
+    {
+        if (_contentGrid != null) _contentGrid.IsVisible = false;
+        if (_mainMenu != null) _mainMenu.IsVisible = true;
     }
 }
