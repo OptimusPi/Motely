44b15135b9be56d74e8f5a2ae8d3e19639a04f5f
Nathanial P. Howard <admin@optimuspi.com>
2025-08-19 19:55:27 -0500

    tech debt

--- Motely/MotelySingleSearchContext.BuffoonHelper.cs
+++
@@ -1,2 +0,0 @@
-// (Intentionally left blank) Previously contained temporary Buffoon helper that created a fresh stream per call.
-// Removed in favor of persistent per-ante Buffoon joker stream usage via GetNextBuffoonPackContents(ref MotelySingleJokerStream, ...).
