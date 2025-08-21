5696ddcb3248d20242902149f1daadc67f974285
Nathanial P. Howard <admin@optimuspi.com>
2025-08-20 20:37:01 -0500

    break everything because I'm stupid

---
+++ Motely/MotelyVectorSearchContext.Shop.cs
@@ -0,0 +1,148 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace Motely;
+
+public ref struct MotelyVectorShopItemStream
+{
+    public MotelyVectorPrngStream ItemTypeStream;
+    public MotelyVectorJokerStream JokerStream;
+    public MotelyVectorTarotStream TarotStream;
+    public MotelyVectorPlanetStream PlanetStream;
+    public MotelyVectorSpectralStream SpectralStream;
+    
+    public Vector512<double> TarotRate;
+    public Vector512<double> PlanetRate;
+    public Vector512<double> PlayingCardRate;
+    public Vector512<double> SpectralRate;
+    public Vector512<double> TotalRate;
+    
+    public readonly bool DoesProvideJokers => !JokerStream.IsInvalid;
+    public readonly bool DoesProvideTarots => !TarotStream.IsInvalid;
+    public readonly bool DoesProvidePlanets => !PlanetStream.IsInvalid;
+    public readonly bool DoesProvideSpectrals => !SpectralStream.IsInvalid;
+}
+
+ref partial struct MotelyVectorSearchContext
+{
+    private const int ShopJokerRate = 20;
+
+    public MotelyVectorShopItemStream CreateShopItemStream(int ante, 
+        MotelyShopStreamFlags flags = MotelyShopStreamFlags.Default,
+        MotelyJokerStreamFlags jokerFlags = MotelyJokerStreamFlags.Default,
+        bool isCached = false)
+    {
+        return CreateShopItemStream(ante, Deck.GetDefaultRunState(), flags, jokerFlags, isCached);
+    }
+    
+    public MotelyVectorShopItemStream CreateShopItemStream(int ante,
+        MotelyRunState runState,
+        MotelyShopStreamFlags flags = MotelyShopStreamFlags.Default,
+        MotelyJokerStreamFlags jokerFlags = MotelyJokerStreamFlags.Default,
+        bool isCached = false)
+    {
+        MotelyVectorShopItemStream stream = new()
+        {
+            ItemTypeStream = CreatePrngStream(MotelyPrngKeys.ShopItemType + ante, isCached),
+            JokerStream = flags.HasFlag(MotelyShopStreamFlags.ExcludeJokers) ?
+                default : CreateShopJokerStream(ante, jokerFlags, isCached),
+            TarotStream = flags.HasFlag(MotelyShopStreamFlags.ExcludeTarots) ?
+                default : CreateShopTarotStream(ante, isCached),
+            PlanetStream = flags.HasFlag(MotelyShopStreamFlags.ExcludePlanets) ?
+                default : CreateShopPlanetStream(ante, isCached),
+            SpectralStream = flags.HasFlag(MotelyShopStreamFlags.ExcludeSpectrals) || Deck != MotelyDeck.Ghost ?
+                default : MotelyVectorSearchContext.CreateShopSpectralStream(ante, isCached),
+                
+            TarotRate = Vector512.Create(4.0),
+            PlanetRate = Vector512.Create(4.0),
+            PlayingCardRate = Vector512.Create(0.0),
+            SpectralRate = Vector512.Create(0.0)
+        };
+        
+        if (Deck == MotelyDeck.Ghost)
+        {
+            stream.SpectralRate = Vector512.Create(2.0);
+        }
+        
+        if (runState.IsVoucherActive(MotelyVoucher.TarotTycoon))
+        {
+            stream.TarotRate = Vector512.Create(32.0);
+        }
+        else if (runState.IsVoucherActive(MotelyVoucher.TarotMerchant))
+        {
+            stream.TarotRate = Vector512.Create(9.6);
+        }
+        
+        if (runState.IsVoucherActive(MotelyVoucher.PlanetTycoon))
+        {
+            stream.PlanetRate = Vector512.Create(32.0);
+        }
+        else if (runState.IsVoucherActive(MotelyVoucher.PlanetMerchant))
+        {
+            stream.PlanetRate = Vector512.Create(9.6);
+        }
+        
+        if (runState.IsVoucherActive(MotelyVoucher.MagicTrick))
+        {
+            stream.PlayingCardRate = Vector512.Create(4.0);
+        }
+        
+        stream.TotalRate = Vector512.Create((double)ShopJokerRate) + stream.TarotRate + stream.PlanetRate + stream.PlayingCardRate + stream.SpectralRate;
+        
+        return stream;
+    }
+    
+#if !DEBUG
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public MotelyItemVector GetNextShopItem(ref MotelyVectorShopItemStream stream)
+    {
+        Vector512<double> itemTypePoll = GetNextRandom(ref stream.ItemTypeStream) * stream.TotalRate;
+        Vector512<double> shopJokerRateVector = Vector512.Create((double)ShopJokerRate);
+        
+        // Check if it's a joker
+        Vector512<double> isJoker = Vector512.LessThan(itemTypePoll, shopJokerRateVector);
+        MotelyItemVector jokerResult = stream.DoesProvideJokers ? 
+            GetNextJoker(ref stream.JokerStream) : 
+            new MotelyItemVector(new MotelyItem(MotelyItemType.JokerExcludedByStream));
+            
+        itemTypePoll -= shopJokerRateVector;
+        
+        // Check if it's a tarot
+        Vector512<double> isTarot = Vector512.LessThan(itemTypePoll, stream.TarotRate);
+        MotelyItemVector tarotResult = stream.DoesProvideTarots ?
+            GetNextTarot(ref stream.TarotStream) :
+            new MotelyItemVector(new MotelyItem(MotelyItemType.TarotExcludedByStream));
+            
+        itemTypePoll -= stream.TarotRate;
+        
+        // Check if it's a planet
+        Vector512<double> isPlanet = Vector512.LessThan(itemTypePoll, stream.PlanetRate);
+        MotelyItemVector planetResult = stream.DoesProvidePlanets ?
+            GetNextPlanet(ref stream.PlanetStream) :
+            new MotelyItemVector(new MotelyItem(MotelyItemType.PlanetExcludedByStream));
+            
+        itemTypePoll -= stream.PlanetRate;
+        
+        // Check if it's a playing card
+        Vector512<double> isPlayingCard = Vector512.LessThan(itemTypePoll, stream.PlayingCardRate);
+        MotelyItemVector playingCardResult = new MotelyItemVector(new MotelyItem(MotelyItemType.NotImplemented));
+        
+        // Otherwise it's a spectral
+        MotelyItemVector spectralResult = stream.DoesProvideSpectrals ?
+            MotelyVectorSearchContext.GetNextSpectral(ref stream.SpectralStream) :
+            new MotelyItemVector(new MotelyItem(MotelyItemType.SpectralExcludedByStream));
+            
+        // Select the appropriate result based on item type
+        MotelyItemVector result = new MotelyItemVector(Vector256.ConditionalSelect(
+            MotelyVectorUtils.ShrinkDoubleMaskToInt(isJoker), jokerResult.Value,
+            Vector256.ConditionalSelect(
+                MotelyVectorUtils.ShrinkDoubleMaskToInt(isTarot), tarotResult.Value,
+                Vector256.ConditionalSelect(
+                    MotelyVectorUtils.ShrinkDoubleMaskToInt(isPlanet), planetResult.Value,
+                    Vector256.ConditionalSelect(
+                        MotelyVectorUtils.ShrinkDoubleMaskToInt(isPlayingCard), playingCardResult.Value, spectralResult.Value)))));
+                     
+        return result;
+    }
+}
--- Motely/MotelyVectorSearchContext.Spectrals.cs
+++ Motely/MotelyVectorSearchContext.Spectrals.cs
@@ -1,23 +1,90 @@
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Reflection.Metadata;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics;
 
 namespace Motely;
 
-ref partial struct MotelyVectorSearchContext
+public ref struct MotelyVectorSpectralStream(MotelyVectorResampleStream resampleStream, MotelyVectorPrngStream soulBlackHolePrngStream)
 {
+    public readonly bool IsNull => ResampleStream.IsInvalid;
+    public MotelyVectorResampleStream ResampleStream = resampleStream;
+    public MotelyVectorPrngStream SoulBlackHolePrngStream = soulBlackHolePrngStream;
+    public readonly bool IsSoulBlackHoleable => !SoulBlackHolePrngStream.IsInvalid;
+
 #if !DEBUG
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    private MotelyVectorPrngStream CreateSpectralStreamCached(int ante, string source)
+    private static MotelyVectorSpectralStream CreateSpectralStream(string source, int ante, bool searchSpectral, bool soulBlackHoleable, bool isCached)
     {
-        return CreatePrngStream(MotelyPrngKeys.Spectral + source + ante, true);
+        return new(
+            searchSpectral ? CreateResampleStream(MotelyPrngKeys.Spectral + source + ante, isCached) : MotelyVectorResampleStream.Invalid,
+            soulBlackHoleable ? CreatePrngStream(MotelyPrngKeys.SpectralSoulBlackHole + MotelyPrngKeys.Spectral + ante, isCached) : MotelyVectorPrngStream.Invalid
+        );
     }
 
 #if !DEBUG
     [MethodImpl(MethodImplOptions.AggressiveInlining)]
 #endif
-    private MotelyVectorPrngStream CreateSpectralStream(int ante, string source)
+    public static MotelyVectorSpectralStream CreateShopSpectralStream(int ante, bool isCached = false) =>
+        CreateSpectralStream(MotelyPrngKeys.ShopItemSource, ante, true, false, isCached);
+
+#if !DEBUG
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+#endif
+    public MotelyItemVector GetNextSpectral(ref MotelyVectorSpectralStream stream)
     {
-        return CreatePrngStream(MotelyPrngKeys.Spectral + source + ante);
+        if (stream.IsNull) return new MotelyItemVector(Vector256<int>.Zero);
+
+        Vector256<int> items = Vector256<int>.Zero;
+        Vector512<double> activeMask = Vector512.Create(1.0);
+
+        if (stream.IsSoulBlackHoleable)
+        {
+            Vector512<double> randomSoul = GetNextRandom(ref stream.SoulBlackHolePrngStream, activeMask);
+            Vector512<double> maskSoul = Vector512.GreaterThan(randomSoul, Vector512.Create(0.997));
+            Vector256<int> maskSoulInt = MotelyVectorUtils.ExtendDoubleMaskToInt(maskSoul);
+            items = Vector256.ConditionalSelect(maskSoulInt, Vector256.Create((int)MotelyItemType.Soul), items);
+            activeMask = Vector512.AndNot(activeMask, maskSoul);
+
+            if (!Vector512.EqualsAll(activeMask, Vector512<double>.Zero))
+            {
+                Vector512<double> randomBH = GetNextRandom(ref stream.SoulBlackHolePrngStream, activeMask);
+                Vector512<double> maskBH = Vector512.GreaterThan(randomBH, Vector512.Create(0.997));
+                Vector256<int> maskBHInt = MotelyVectorUtils.ExtendDoubleMaskToInt(maskBH);
+                items = Vector256.ConditionalSelect(maskBHInt, Vector256.Create((int)MotelyItemType.BlackHole), items);
+                activeMask = Vector512.AndNot(activeMask, maskBH);
+            }
+        }
+
+        if (Vector512.EqualsAll(activeMask, Vector512<double>.Zero))
+            return new MotelyItemVector(items);
+
+        Vector256<int> spectralEnums = GetNextRandomInt(ref stream.ResampleStream.InitialPrngStream, 0, MotelyEnum<MotelySpectralCard>.ValueCount, activeMask);
+        Vector256<int> spectralItems = Vector256.BitwiseOr(spectralEnums, Vector256.Create((int)MotelyItemTypeCategory.SpectralCard));
+        items = Vector256.ConditionalSelect(MotelyVectorUtils.ExtendDoubleMaskToInt(activeMask), spectralItems, items);
+
+        int resampleCount = 0;
+        while (true)
+        {
+            Vector256<int> resampleMaskInt = Vector256.Equals(items, Vector256.Create((int)MotelyItemType.Soul)) |
+                                             Vector256.Equals(items, Vector256.Create((int)MotelyItemType.BlackHole));
+            if (Vector256.EqualsAll(resampleMaskInt, Vector256<int>.Zero)) break;
+
+            Vector512<double> resampleMask = MotelyVectorUtils.ExtendIntMaskToDouble(resampleMaskInt);
+            Vector256<int> newEnums = GetNextRandomInt(
+                ref GetResamplePrngStream(ref stream.ResampleStream, stream.ResampleKey, resampleCount),
+                0, MotelyEnum<MotelySpectralCard>.ValueCount,
+                resampleMask
+            );
+            Vector256<int> newItems = Vector256.BitwiseOr(newEnums, Vector256.Create((int)MotelyItemTypeCategory.SpectralCard));
+            items = Vector256.ConditionalSelect(resampleMaskInt, newItems, items);
+            ++resampleCount;
+        }
+
+        return new MotelyItemVector(items);
     }
 
     // Helper method for filtering spectral cards in vector context
