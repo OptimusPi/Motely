6e470dfbc732639b6e3267fcba64261b715d85b4
Nathanial P. Howard <admin@optimuspi.com>
2025-08-27 17:24:10 -0500

    Create PassthroughFilterDesc.cs

---
+++ Motely/filters/PassthroughFilterDesc.cs
@@ -0,0 +1,21 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.Intrinsics;
+
+namespace Motely;
+
+public struct PassthroughFilterDesc() : IMotelySeedFilterDesc<PassthroughFilterDesc.PassthroughFilter>
+{
+
+    public PassthroughFilter CreateFilter(ref MotelyFilterCreationContext ctx)
+    {
+        return new PassthroughFilter();
+    }
+
+    public struct PassthroughFilter() : IMotelySeedFilter
+    {
+        public readonly VectorMask Filter(ref MotelyVectorSearchContext searchContext)
+        {
+            return VectorMask.AllBitsSet;
+        }
+    }
+}
