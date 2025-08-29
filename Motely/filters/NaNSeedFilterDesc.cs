b10a18fed75abc9250d608a6714893ca48f1d1a7
Nathanial P. Howard <admin@optimuspi.com>
2025-08-28 20:54:00 -0500

    Update NaNSeedFilterDesc.cs

--- Motely/filters/NaNSeedFilterDesc.cs
+++ Motely/filters/NaNSeedFilterDesc.cs
@@ -9,59 +9,54 @@ public struct NaNSeedFilterDesc : IMotelySeedFilterDesc<NaNSeedFilterDesc.NaNSee
     public NaNSeedFilterDesc()
     {
         PseudoHashKeys = [
-        // Joker probability/math operations
-        // "lucky_money",
-        // "lucky_mult", 
-        // "misprint",
-        // "bloodstone",
-        // "parking",
-        // "business",
-        // "space",
-        // "8ball",
-        // "halu",
-        // "gros_michel",
-        // "cavendish",
-
-        // // Boss blind effects
-        // "wheel",
-        // "hook",
-        // "cerulean_bell",
-        // "crimson_heart",
-
-        // // Card modifications
-        // "wheel_of_fortune",
-        // "invisible",
-        // "perkeo",
-        // "madness",
-        // "ankh_choice",
-
-        // // Tarot/Spectral effects
-        // "sigil",
-        // "ouija",
-        // "familiar_create",
-        // "grim_create", 
-        // "incantation_create",
-        // //"random_destroy",
-        // "spe_card",
-
-        // // Pack/shop generation
-        // "stdset",
-        // "stdseal",
-        // "stdsealtype",
-        // "omen_globe",
-        // "cert_fr",
-        // "certsl",
-
-        // // Other mechanics
-        // "flipped_card",
-        // "to_do",
-        // "erratic",
-        // "edition_deck"
-
-
-
+        //Joker probability/math operations
         "lucky_money",
-        "wheel"
+        "lucky_mult", 
+        "misprint",
+        "bloodstone",
+        "parking",
+        "business",
+        "space",
+        "8ball",
+        "halu",
+        "gros_michel",
+        "cavendish",
+
+        // Boss blind effects
+        "wheel",
+        "hook",
+        "cerulean_bell",
+        "crimson_heart",
+
+        // Card modifications
+        "wheel_of_fortune",
+        "invisible",
+        "perkeo",
+        "madness",
+        "ankh_choice",
+
+        // Tarot/Spectral effects
+        "sigil",
+        "ouija",
+        "familiar_create",
+        "grim_create", 
+        "incantation_create",
+        "random_destroy",
+        "spe_card",
+
+        // Pack/shop generation
+        "stdset",
+        "stdseal",
+        "stdsealtype",
+        "omen_globe",
+        "cert_fr",
+        "certsl",
+
+        // Other mechanics
+        "flipped_card",
+        "to_do",
+        "erratic",
+        "edition_deck"
         ];
     }
 
@@ -85,8 +80,6 @@ public struct NaNSeedFilterDesc : IMotelySeedFilterDesc<NaNSeedFilterDesc.NaNSee
         {
             // Check each PRNG key for NaN values
             VectorMask resultMask = VectorMask.NoBitsSet;
-            VectorMask resultMask2 = VectorMask.NoBitsSet;
-            VectorMask resultMask3 = VectorMask.NoBitsSet;
 
             for (int i = 0; i < PseudoHashKeys.Length; i++)
             {
@@ -95,28 +88,22 @@ public struct NaNSeedFilterDesc : IMotelySeedFilterDesc<NaNSeedFilterDesc.NaNSee
                 Vector512<double> prng = searchContext.GetNextPrngState(ref stream);
 
                 // Magic numbers match any seeds
-                resultMask |= Vector512.Equals(prng, pointThreeTwo);
-                resultMask2 |= Vector512.Equals(prng, pointSixEight);
+                resultMask |= Vector512.Equals(stream.State, pointThreeTwo);
 
-
-                // Log if we found Magic Number Loop for this key
                 if (resultMask.IsPartiallyTrue())
                 {
-                    Console.WriteLine($"\n🎰💥'{key}' produced a Magic Number Loop! (0.3211483013596)");
-                }
-                if (resultMask2.IsPartiallyTrue())
-                {
-                    Console.WriteLine($"\n🎰💥'{key}' produced a Magic Number Loop! (0.6882134081454)");
+                    Console.WriteLine($"\n🎰💥'{key}' PRNG Stream.State=0.3211483013596");
                 }
 
-                resultMask3 |= ~Vector512.Equals(prng, prng);
-                if (resultMask3.IsPartiallyTrue())
+                resultMask |= Vector512.Equals(stream.State, pointSixEight);
+
+                if (resultMask.IsPartiallyTrue())
                 {
-                    Console.WriteLine($"\n🎰💥'{key}' produced a NaN!");
+                    Console.WriteLine($"\n🎰💥'{key}' PRNG Stream.State=0.6882134081454");
                 }
             }
-
-            return resultMask | resultMask2 | resultMask3;
+            
+            return resultMask;
         }
     }
 }
