263f7792d4a2d91f68a826640fcc3ceb9c1f099f
Nathanial P. Howard <admin@optimuspi.com>
2025-09-05 04:29:41 -0500

    good job pifreak

--- Motely/Utils/FilterCategoryMapper.cs
+++ Motely/Utils/FilterCategoryMapper.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using Motely.Filters;
 
 namespace Motely.Utils
---
+++ Motely/filters/MotelyJson/FilterCategory.cs
@@ -0,0 +1,17 @@
+namespace Motely.Filters;
+
+/// <summary>
+/// Categories for specialized JSON filter implementations
+/// </summary>
+public enum FilterCategory
+{
+    Voucher,
+    Boss,
+    Tag,
+    TarotCard,
+    PlanetCard, 
+    SpectralCard,
+    PlayingCard,
+    Joker,
+    SoulJoker
+}
