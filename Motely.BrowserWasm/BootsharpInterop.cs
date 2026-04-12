470ad961076b9a7ad676bdf102308f07ceec2591
Nathanial P. Howard <admin@optimuspi.com>
2026-04-11 19:54:41 -0500

    k

--- Motely.BrowserWasm/BootsharpInterop.cs
+++ Motely.BrowserWasm/BootsharpInterop.cs
@@ -5,13 +5,13 @@ using Bootsharp.Inject;
 [assembly: JSExport(
     typeof(Motely.BrowserWasm.IMotelySingleSearchContext),
     typeof(Motely.Analysis.IMotelySingleSearchContextImpl),
-    typeof(Motely.BrowserWasm.IMotelyWasmHost),
     typeof(MotelyJaml.IMotelyJamlSearchBuilder),
     typeof(Motely.BrowserWasm.IMotelySearchSession))]
 [assembly: JSImport([typeof(Motely.BrowserWasm.ISearchEvents)])]
 [assembly: JSPreferences(
     Space = [
         @"^Motely\.BrowserWasm\.(\S+)", "$1",
+        @"^Motely\.Analysis\.(\S+)", "$1",
         @"^Motely\.(\S+)", "$1",
         @"^MotelyJaml\.(\S+)", "$1"
     ]
