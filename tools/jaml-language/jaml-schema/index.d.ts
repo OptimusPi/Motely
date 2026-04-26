21a0d68f61114d6f88134b5344d2db2f8268a0de
Nathanial P. Howard <admin@optimuspi.com>
2026-04-26 01:28:44 -0500

    fuck it i dotn get vsx

--- tools/jaml-language/jaml-schema/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const schema: Record<string, unknown>;
-declare const rootKeys: readonly string[];
-declare const clauseKeys: readonly string[];
-
-export default schema;
-export { clauseKeys, rootKeys, schema };
