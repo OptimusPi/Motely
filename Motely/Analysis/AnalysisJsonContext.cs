6a1bc4b8f5c16ff14f3017b62f121b622dd435e4
Nathanial P. Howard <admin@optimuspi.com>
2026-05-27 09:12:09 -0500

    Fix WASM compiler errors, restore GetBossForAnte, and add JSON serialization types for Jamlyzer

--- Motely/Analysis/AnalysisJsonContext.cs
+++ Motely/Analysis/AnalysisJsonContext.cs
@@ -10,4 +10,6 @@ namespace Motely.Analysis;
 [JsonSerializable(typeof(MotelyAnteAnalysis))]
 [JsonSerializable(typeof(MotelyAnalyzedItem))]
 [JsonSerializable(typeof(MotelyBoosterPackAnalysis))]
+[JsonSerializable(typeof(MotelyJamlyzerResult))]
+[JsonSerializable(typeof(MotelyJamlyzerSeedResult))]
 public partial class AnalysisJsonContext : JsonSerializerContext { }
