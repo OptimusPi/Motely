77bf9f0e3eaad56e0fe236697499434d527e2dc3
Nathanial P. Howard <admin@optimuspi.com>
2026-05-29 20:58:14 -0500

    I am a ghost. I am invisible :)

--- Motely.Wasm/GlobalUsings.cs
+++
@@ -1,7 +0,0 @@
-global using Motely;
-global using Motely.Analysis;
-global using Motely.Enums;
-global using Motely.Filters;
-global using Motely.Filters.Jaml;
-global using Motely.Filters.Jaml.Converters;
-global using Motely.Filters.Native;
