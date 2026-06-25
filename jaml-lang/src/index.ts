9f50bb5ac3e16363c03e7dd7a1e30eaa317f8793
Nathanial P. Howard <admin@optimuspi.com>
2026-06-25 16:21:02 -0500

    rwemove wasm and jaml-lang temporarily

--- jaml-lang/src/index.ts
+++
@@ -1,5 +0,0 @@
-export { validate, getDiagnostics, Severity, type Diagnostic, type LspDiagnostic } from "./validator.js";
-export { getCompletions, type CompletionItem, type CompletionKind } from "./completions.js";
-export { getHover, type HoverInfo } from "./hover.js";
-export { getContext, type JamlContext, type JamlContextKind } from "./context.js";
-export * as Vocab from "./generated.js";
